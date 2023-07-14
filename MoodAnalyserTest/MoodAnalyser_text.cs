888735ac6ab6e4a6e5d3b78fb348195086d9eb48
Ankush Bahekar <ankushmb9@gmail.com>
2023-07-14 08:14:11 +0530

    [Ankush] Add:Create Sad method using MoodAnaluser.

---
+++ MoodAnalyserTest/MoodAnalyser_text.cs
@@ -0,0 +1,22 @@
+﻿using MoodAnalyzerProblem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserTest
+{
+    public class MoodAnalyser_text
+    {
+        MoodAnalyse moodAnalyse= new MoodAnalyse();
+        [Test]
+        public void GivenSadMood_ShouldReturnSad()
+        {
+            string Result = moodAnalyse.AnalyseMood("I am in Sad Mood");
+            Assert.AreEqual(Result, "Sad");
+
+        }
+    }
+
+}
